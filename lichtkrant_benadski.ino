bc603194888b353ea1ae50b8a1c4cf2087b862c7
Sebastius <bas@knutselaar.net>
2016-09-10 17:21:08 +0200

    NTP ingebouwd
    Nu dus ook de tijd!

--- lichtkrant_benadski.ino
+++ lichtkrant_benadski.ino
@@ -20,6 +20,14 @@ Timezone myTZ(CEST, CET);
 TimeChangeRule *tcr;        //pointer to the time change rule, use to get TZ abbrev
 time_t utc, local;
 
+// NTP Server settings and preparations
+unsigned int localPort = 2390;      // local port to listen for UDP packets
+IPAddress timeServerIP; // time.nist.gov NTP server address
+const char* ntpServerName = "pool.ntp.org";
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+WiFiUDP udp;
+
 #define SoftSerialTX 13
 #define SoftSerialRX 4
 #define Button 0
@@ -32,6 +40,7 @@ char msg[50];
 int value = 0;
 
 bool spacestate;
+bool klok_ok = false;
 
 //define your default values here, if there are different values in config.json, they are overwritten.
 char mqtt_server[40];
@@ -98,6 +107,25 @@ const char endprogram[] = {
   0xBF, 0xB1,
 };
 
+#define in_left 0x80
+#define in_right 0x81
+#define in_upwards 0x82
+#define in_downwards 0x83
+#define open_center 0x84
+#define close_center 0x85
+#define open_left 0x86
+#define close_left 0x87
+#define in_shift 0x88 // vanuit het midden
+#define out_squeeze 0x89 // naar het midden
+#define flash 0x8A
+#define jump 0x8B // jump on screen and stay
+#define Doff 0x8C
+#define Dobig 0x8D
+#define clearscreen 0x8E
+#define showclock 0x8F
+#define snow 0x90
+#define dsnow 0x91
+#define randomeffect 0xA3
 
 void snelheid(byte snelheid) {
   verstuurbyte(0xA0);
@@ -108,6 +136,11 @@ void snelheid(byte snelheid) {
     snelheid = 9;
   }
   verstuurbyte(snelheid + 30);
+}
+
+void effectje(char programma, char parameter){
+  verstuurbyte(programma);
+  verstuurbyte(parameter);
 }
 
 void programmakeuze(char programma) {
@@ -249,8 +282,6 @@ void setup() {
           Serial.println(mqtt_server);
 
           strcpy(mqtt_port, json["mqtt_port"]);
-          strcpy(location, json["location"]);
-          strcpy(id, json["id"]);
           strcpy(debugtopic, json["debugtopic"]);
           strcpy(pubtopic, json["pubtopic"]);
         } else {
@@ -270,8 +301,6 @@ void setup() {
   WiFiManager wifiManager;
   wifiManager.addParameter(&custom_mqtt_server);
   wifiManager.addParameter(&custom_mqtt_port);
-  wifiManager.addParameter(&custom_location);
-  wifiManager.addParameter(&custom_id);
   wifiManager.addParameter(&custom_debug);
   wifiManager.addParameter(&custom_pub);
 
@@ -291,20 +320,23 @@ void setup() {
   //if you get here you have connected to the WiFi
   Serial.println("connected...yeey :)");
   bamtext("connected...yeey :)",  'A', blackred);
-  
+
   updateParameters();
 
 
 
   //Serial.println("local ip");  Serial.println(WiFi.localIP();
-  
+
   Serial.println();
   Serial.print("MQTT Server: ");
   Serial.println(mqtt_server);
-  
+
   Serial.println();
   client.setServer(mqtt_server, 1883);
   client.setCallback(onMqttMessage);
+
+  udp.begin(localPort);
+  ntpsync();
 }
 
 
@@ -327,6 +359,14 @@ void loop() {
 
   }
 
+  if (hour(now()) % 3 == 0 && minute(now()) == 0 && second(now()) == 0) {
+    klok_ok = false;
+  }
+
+  if (!klok_ok) {
+    ntpsync();
+  }
+
 }
 
 
@@ -342,8 +382,6 @@ void updateParameters() {
     //read updated parameters
     strcpy(mqtt_server, custom_mqtt_server.getValue());
     strcpy(mqtt_port, custom_mqtt_port.getValue());
-    strcpy(location, custom_location.getValue());
-    strcpy(id, custom_id.getValue());
     strcpy(debugtopic, custom_debug.getValue());
     strcpy(pubtopic, custom_pub.getValue());
 
@@ -354,8 +392,6 @@ void updateParameters() {
     JsonObject& json = jsonBuffer.createObject();
     json["mqtt_server"] = mqtt_server;
     json["mqtt_port"] = mqtt_port;
-    json["location"] = location;
-    json["id"] = id;
     json["debugtopic"] = debugtopic;
     json["pubtopic"] = pubtopic;
 
@@ -374,8 +410,6 @@ void ondemandPortal() {
   WiFiManager wifiManager;
   wifiManager.addParameter(&custom_mqtt_server);
   wifiManager.addParameter(&custom_mqtt_port);
-  wifiManager.addParameter(&custom_location);
-  wifiManager.addParameter(&custom_id);
   wifiManager.addParameter(&custom_debug);
   wifiManager.addParameter(&custom_pub);
   wifiManager.setSaveConfigCallback(saveConfigCallback);
@@ -394,10 +428,11 @@ boolean reconnect() {
   if (client.connect(chipidchar)) {
     Serial.println("Reconnected to MQTT");
     // Once connected, publish an announcement...
-    mqtt_publish(String(debugtopic) + "/" + location + "/" + id + "/" + chipid, swversion + String(" ") + WiFi.localIP().toString() + String(" ") + String(MAC_char));
-
+    mqtt_publish(String(debugtopic) + "/11/" + chipid, swversion + String(" ") + WiFi.localIP().toString() + String(" ") + String(MAC_char));
     client.subscribe("revspace/button/#");
     client.subscribe("revspace/state");
+    client.subscribe("revspace/sensors/temperature/11/1");
+    
     client.loop();
   }
   return client.connected();
@@ -457,7 +492,7 @@ void onMqttMessage(char* topic, byte * payload, unsigned int length) {
 
   if (strcmp(topic, "revspace/button/rollcall") == 0) {
     Serial.println("Rollcall!");
-    mqtt_publish(String(debugtopic) + "/" + location + "/" + chipid + "/" + location, swversion + String(" ") + WiFi.localIP().toString() + String(" ") + String(MAC_char));
+    mqtt_publish(String(debugtopic) + "/11/" + chipid, swversion + String(" ") + WiFi.localIP().toString() + String(" ") + String(MAC_char));
   }
 }
 
@@ -473,4 +508,77 @@ void mqtt_publish (String topic, String message) {
   message.toCharArray(m, sizeof m);
 
   client.publish(t, m, /*retain*/ 1);
+}
+
+boolean ntpsync() {
+
+  //get a random server from the pool
+  WiFi.hostByName(ntpServerName, timeServerIP);
+
+  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+
+  delay(500);
+
+  int cb = udp.parsePacket();
+  if (!cb) {
+    Serial.println("no packet yet");
+    klok_ok = false;
+  }
+  else {
+    Serial.print("packet received, length=");
+    Serial.println(cb);
+    // We've received a packet, read the data from it
+    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+    //the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, esxtract the two words:
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+    Serial.print("Seconds since Jan 1 1900 = " );
+    Serial.println(secsSince1900);
+
+    // now convert NTP time into everyday time:
+    Serial.print("Unix time = ");
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
+    unsigned long epoch = secsSince1900 - seventyYears;
+    // print Unix time:
+    Serial.println(epoch);
+
+    local = myTZ.toLocal(epoch, &tcr);
+
+    setTime(local);
+    klok_ok = true;
+    klokinstellen(now());
+  }
+}
+
+// send an NTP request to the time server at the given address
+unsigned long sendNTPpacket(IPAddress& address)
+{
+  Serial.println("sending NTP packet...");
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  udp.beginPacket(address, 123); //NTP requests are to port 123
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
 }
